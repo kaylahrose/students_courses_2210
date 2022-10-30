6668158a142a3c9ddf0862295fd547504e87a58d
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-10-30 14:56:26 -0700

    Create student class

---
+++ lib/student.rb
@@ -0,0 +1,17 @@
+class Student
+  attr_reader :name, :age, :scores
+
+  def initialize(info)
+    @name = info[:name]
+    @age = info[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    scores << score
+  end
+
+  def grade
+    scores.sum(0.0) / scores.size
+  end
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,23 @@
 require './lib/student'
 
 RSpec.describe Student do
+  let(:student) { Student.new({ name: 'Morgan', age: 21 }) }
+
+  it 'exists and has attributes' do
+    expect(student.name).to eq('Morgan')
+    expect(student.age).to eq(21)
+    expect(student.scores).to eq([])
+  end
+
+  it 'logs scores' do
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.scores).to eq([89, 78])
+  end
+
+  it 'calculates average grade' do
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.grade).to eq(83.5)
+  end
 end
